8c4b7ae51fffbdc5dbcd9befd8d771927337b898
Maksim <max.godnavi133@gmail.com>
2024-05-22 03:26:59 +0300

    Auth almost done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import { Blocks } from 'react-loader-spinner';
 import Header from './Header/Header';
+import LoginPage from 'pages/LogIn/LogIn';
+import RegisterPage from 'pages/Register/Register';
+import RestrictedRoute from './Routes/RestrictedRoute';
 const Home = lazy(() => import('pages/Home'));
 const MovieInfo = lazy(() => import('pages/MovieInfo'));
 const Search = lazy(() => import('pages/Search'));
@@ -22,6 +25,22 @@ export const App = () => {
       >
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute>
+                <LoginPage />
+              </RestrictedRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute>
+                <RegisterPage />
+              </RestrictedRoute>
+            }
+          />
           <Route path="/movies" element={<Search />} />
           <Route path="/movies/:movieId/*" element={<MovieInfo />} />
         </Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Acsent, Logo, StyledHeared, StyledNav } from './Header.styled';
+import { useSelector } from 'react-redux';
+import { selectAuthAuthenticated } from '../../redux/authorization/auth.selectors';
 
 const Header = () => {
+  const authenticated = useSelector(selectAuthAuthenticated);
+
   return (
     <StyledHeared>
       <Logo to={'/'}>
@@ -15,6 +19,17 @@ const Header = () => {
         <NavLink to={'/movies'} className="header-link">
           Movies
         </NavLink>
+
+        {!authenticated && (
+          <>
+            <NavLink to={'/login'} className="header-link">
+              LogIn
+            </NavLink>
+            <NavLink to={'/register'} className="header-link">
+              Register
+            </NavLink>
+          </>
+        )}
       </StyledNav>
     </StyledHeared>
   );
